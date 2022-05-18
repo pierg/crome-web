16daaa313e28e632837f996f50bb639554051d17
Ronzon Mathis <mathis.ronzon@ecole.ensicaen.Fr>
2022-05-18 18:11:57 +0200

    Update json file of goals with their LTL value

--- backend/operations/modelling.py
+++ backend/operations/modelling.py
@@ -115,122 +115,127 @@ class Modelling:
         w = load_world(project_folder)
 
         goal_path = Path(os.path.join(project_folder, f"goals/{goal_file}"))
+        json_obj = None
         with open(goal_path) as json_file:
             json_obj = json.load(json_file)
-            contract_names = ["assumptions", "guarantees"]
-            contract_lists = [[], []]  # type: list[list[LTL]]
-            for i in range(len(contract_lists)):
-                for contract_element in json_obj["contract"][contract_names[i]]:
-                    if "pattern" in contract_element:
-                        args = contract_element["pattern"]["arguments"]
-                        if len(args) == 1:
-                            if type(args[0]["value"]) == list:
-                                list_of_locations = []
-                                typeset_location = set()
-                                for location in args[0]["value"]:
-                                    list_of_locations.append(location)
-                                    typeset_location.add(w.typeset[location])
-                                contract_lists[i].append(
-                                    LTL(
-                                        _init_formula=globals()[
-                                            contract_element["pattern"]["name"]
-                                        ](locations=list_of_locations).__str__(),
-                                        _typeset=Typeset(typeset_location),
-                                    )
-                                )
-                            else:
-                                contract_lists[i].append(
-                                    LTL(
-                                        _init_formula=globals()[
-                                            contract_element["pattern"]["name"]
-                                        ](locations=[w[args[0]["value"]]]).__str__(),
-                                        _typeset=Typeset(
-                                            {w.typeset[args[0]["value"]]}
-                                        ),
-                                    )
-                                )
-                        elif len(args) == 2:
+        contract_names = ["assumptions", "guarantees"]
+        contract_lists = [[], []]  # type: list[list[LTL]]
+        for i in range(len(contract_lists)):
+            for contract_element in json_obj["contract"][contract_names[i]]:
+                if "pattern" in contract_element:
+                    args = contract_element["pattern"]["arguments"]
+                    if len(args) == 1:
+                        if type(args[0]["value"]) == list:
+                            list_of_locations = []
+                            typeset_location = set()
+                            for location in args[0]["value"]:
+                                list_of_locations.append(location)
+                                typeset_location.add(w.typeset[location])
                             contract_lists[i].append(
                                 LTL(
                                     _init_formula=globals()[
                                         contract_element["pattern"]["name"]
-                                    ](
-                                        pre=w[args[0]["value"]],
-                                        post=w[args[1]["value"]],
-                                    ).__str__(),
-                                    _typeset=Typeset(
-                                        {
-                                            w.typeset[args[0]["value"]],
-                                            w.typeset[args[1]["value"]],
-                                        }
-                                    ),
+                                    ](locations=list_of_locations).__str__(),
+                                    _typeset=Typeset(typeset_location),
                                 )
                             )
                         else:
-                            raise Exception(
-                                "Unknown Pattern, the patterns included only have 1 or 2 arguments",
-                            )
-                    elif "ltl_value" in contract_element:
-                        if "world_values" in contract_element:
-                            values = set()
-                            for array in contract_element["world_values"]:
-                                for value in array:
-                                    values.add(w.typeset[value])
                             contract_lists[i].append(
                                 LTL(
-                                    _init_formula=contract_element["ltl_value"],
-                                    _typeset=Typeset(values),
+                                    _init_formula=globals()[
+                                        contract_element["pattern"]["name"]
+                                    ](locations=[w[args[0]["value"]]]).__str__(),
+                                    _typeset=Typeset(
+                                        {w.typeset[args[0]["value"]]}
+                                    ),
+                                )
+                            )
+                    elif len(args) == 2:
+                        contract_lists[i].append(
+                            LTL(
+                                _init_formula=globals()[
+                                    contract_element["pattern"]["name"]
+                                ](
+                                    pre=w[args[0]["value"]],
+                                    post=w[args[1]["value"]],
+                                ).__str__(),
+                                _typeset=Typeset(
+                                    {
+                                        w.typeset[args[0]["value"]],
+                                        w.typeset[args[1]["value"]],
+                                    }
                                 ),
                             )
-                            # TODO FIX FOR PIER
-                            #  In case the designer enters a LTL (not a Pattern), I have the error saying that
-                            #  Atom must have an attribute 'name' but I don't see how to add it here
-                            # PIER: Fixed, forget about the Atom, just provide a _init_formula and a _typeset (which includes Actions/Sensors/Context etc..)
+                        )
+                    else:
+                        raise Exception(
+                            "Unknown Pattern, the patterns included only have 1 or 2 arguments",
+                        )
+                elif "ltl_value" in contract_element:
+                    if "world_values" in contract_element:
+                        values = set()
+                        for array in contract_element["world_values"]:
+                            for value in array:
+                                values.add(w.typeset[value])
+                        contract_lists[i].append(
+                            LTL(
+                                _init_formula=contract_element["ltl_value"],
+                                _typeset=Typeset(values),
+                            ),
+                        )
 
-            # TODO FIX
-            #  context is an array?
+        context = Context(_init_formula="TRUE")
+        if len(json_obj["context"]) == 1:
+            context = w[json_obj["context"][0]]
+        elif len(json_obj["context"]) > 1:
+            for cont in json_obj["context"]:
+                context &= w[cont]
 
-            context = Context(_init_formula="TRUE")
-            if len(json_obj["context"]) == 1:
-                context = w[json_obj["context"][0]]
-            elif len(json_obj["context"]) > 1:
-                context = w[json_obj["context"]]
+        lists_with_and_operators: list[LTL] = []
+        for i in range(len(contract_lists)):
+            if not contract_lists[i]:
+                lists_with_and_operators.append(LTL("true"))
+                continue
+            lists_with_and_operators.append(contract_lists[i][0])
+            for j in range(1, len(contract_lists[i])):
+                lists_with_and_operators[i] = (
+                    lists_with_and_operators[i] & contract_lists[i][j]
+                )
+        # We update the json file with the new value of the LTL.
 
-            lists_with_and_operators: list[LTL] = []
-            for i in range(len(contract_lists)):
-                if not contract_lists[i]:
-                    lists_with_and_operators.append(LTL("TRUE"))
-                    continue
-                lists_with_and_operators.append(contract_lists[i][0])
-                for j in range(1, len(contract_lists[i])):
-                    lists_with_and_operators[i] = (
-                        lists_with_and_operators[i] & contract_lists[i][j]
-                    )
+        for i in range(len(contract_lists)):
+            for j in range(len(contract_lists[i])):
+                json_content = contract_lists[i][j].export_to_json()
+                json_obj["contract"][contract_names[i]][j]["ltl_value"] = json_content["ltl_value"]
+                json_obj["contract"][contract_names[i]][j]["world_values"] = json_content["world_values"]
 
-            contract = Contract(
-                _assumptions=lists_with_and_operators[0],
-                _guarantees=lists_with_and_operators[1],
-            )
+        # Rewrite the goal inside the json
+        json_file = open(goal_path, "w")
+        json_formatted = json.dumps(json_obj, indent=4, sort_keys=True)
+        json_file.write(json_formatted)
+        json_file.close()
 
-            new_goal = crome_cgg_goal.Goal(
-                description=json_obj["description"],
-                id=goal_id,
-                context=context,
-                world=w,
-                contract=contract,
-            )
+        contract = Contract(
+            _assumptions=lists_with_and_operators[0],
+            _guarantees=lists_with_and_operators[1],
+        )
 
-            # We have to remove the former goals if it is an update
-            if not set_of_goals:
-                print("Here ?")
-                set_of_goals = set()
-                set_of_goals.add(new_goal)
-            else:
-                new_set_of_goals: set[crome_cgg_goal.Goal] = {new_goal}
-                for goal in set_of_goals:
-                    if goal.id != goal_id:
-                        new_set_of_goals.add(goal)
-                set_of_goals = new_set_of_goals
-            print(f"the length of the set_of_goals is : {len(set_of_goals)}")
-            print(goal_id)
-            dump_goals(set_of_goals, project_folder)
+        new_goal = crome_cgg_goal.Goal(
+            description=json_obj["description"],
+            id=goal_id,
+            context=context,
+            world=w,
+            contract=contract,
+        )
+
+        # We have to remove the former goals if it is an update
+        if not set_of_goals:
+            set_of_goals = set()
+            set_of_goals.add(new_goal)
+        else:
+            new_set_of_goals: set[crome_cgg_goal.Goal] = {new_goal}
+            for goal in set_of_goals:
+                if goal.id != goal_id:
+                    new_set_of_goals.add(goal)
+            set_of_goals = new_set_of_goals
+        dump_goals(set_of_goals, project_folder)
