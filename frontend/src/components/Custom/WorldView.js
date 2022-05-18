c90dbb5c2be4cba2ea0df4c78c63a3fa6048ae45
Linot <thealinot@gmail.com>
2022-05-18 12:52:47 +0200

    reduce grid size

--- frontend/src/components/Custom/WorldView.js
+++ frontend/src/components/Custom/WorldView.js
@@ -9,9 +9,9 @@ function WorldView(props) {
 
     return(
         <>
-            <div onClick={props.onClick} className={"w-full lg:w-6/12 xl:w-5/12 mt-8 ml-4 mr-4 px-4 relative flex cursor-pointer flex-col min-w-0 break-words border-lightBlue-700 bg-white rounded mb-6 xl:mb-0 shadow-lg opacity-1 transform duration-300 transition-all ease-in-out "
+            <div onClick={props.onClick} className={"w-full lg:w-4/12 xl:w-3/12 mt-8 ml-4 mr-4 px-4 relative flex cursor-pointer flex-col min-w-0 break-words border-lightBlue-700 bg-white rounded mb-6 xl:mb-0 shadow-lg opacity-1 transform duration-300 transition-all ease-in-out "
                 + (props.selected ? "world-border-8" : "world-border-0")}>
-                <div className="flex-auto p-4 pr-0">
+                <div className="flex-auto p-3 pt-4 pr-0">
                     <div className="flex">
                         <div className="flex flex-wrap w-full">
                             <div className="flex flex-wrap">
@@ -20,10 +20,10 @@ function WorldView(props) {
                                 </div>
                             </div>
                             <div className="flex flex-col flex-wrap w-full">
-                                <div className="relative w-full pr-4 mt-2 mb-2 max-w-full flex-grow flex-1">
-                                <span className="text-md text-blueGray-700">
-                                    {props.description}
-                                </span>
+                                <div className="relative w-full pr-4 mt-2 mb-2 max-w-full flex-grow flex-1 ">
+                                    <span className="text-md text-blueGray-700">
+                                        {props.description}
+                                    </span>
                                 </div>
                                 <div className="relative w-full pr-4 max-w-full flex-grow flex-1"/>
                             </div>
