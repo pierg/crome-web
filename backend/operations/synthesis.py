41ee0cd78184f842f8ffb4e0dbcf23f54f592038
Ronzon Mathis <mathis.ronzon@ecole.ensicaen.Fr>
2022-06-02 18:12:04 +0200

    add name into txt file

--- backend/operations/synthesis.py
+++ backend/operations/synthesis.py
@@ -2,7 +2,7 @@ import os
 from os import walk
 
 from backend.shared.paths import controller_path
-from crome_synthesis.controller import ControllerInfo
+from crome_synthesis.controller import ControllerInfo, _check_header
 
 
 class Synthesis:
@@ -12,12 +12,24 @@ class Synthesis:
         dir_path, dir_names, filenames = next(walk(controller_folder))
         list_examples = []
         for filename in filenames:
-            data = {"id": filename.split(".")[0]}
             info = ControllerInfo.from_file(controller_folder / filename)
-            data["assumptions"] = info.a
-            data["guarantees"] = info.g
-            data["inputs"] = info.i
-            data["outputs"] = info.o
+            name = ""
+            with open(controller_folder / filename, 'r') as ifile:
+                name_found = False
+                for line in ifile:
+                    if name_found:
+                        name = line.strip()
+                        break
+                    line, header = _check_header(line)
+
+                    if not line:
+                        continue
+
+                    elif header:
+                        if line == "**NAME**":
+                            name_found = True
+            data = {"id": name, "assumptions": info.a, "guarantees": info.g, "inputs": info.i,
+                    "outputs": info.o}
             list_examples.append(data)
         return list_examples
 
@@ -36,7 +48,7 @@ class Synthesis:
             if "name" in data:
                 file.write("**NAME**\n\n")
                 file.write(f"{data['name']}\n\n")
-                
+
             file.write("**ASSUMPTIONS**\n\n")
             for assumption in data["assumptions"]:
                 file.write(f"{assumption}\n")
