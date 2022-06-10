a93492f505da00300874e63b815bb362f2dd2ff4
guillaume <gasser.guillaume@gmail.com>
2022-06-10 12:35:34 +0200

    working on Contracts

--- frontend/src/views/custom/CustomContracts.js
+++ frontend/src/views/custom/CustomContracts.js
@@ -1,20 +1,59 @@
 import React from 'react';
 import CustomHeader from "../../components/Crome/CustomHeader";
 import customcontractsheaderscards from "../../_texts/custom/customcontractsheaderscards";
+import customcontractsinfo from "../../_texts/custom/customcontractsinfo";
 
 
 export default class CustomContracts extends React.Component {
+    state = {
+        headerStates: [true, false, false, false, false, false]
+    }
 
+    handleHeaderStates = (indexActiveHeader) => {
+            console.log(indexActiveHeader)
+            if(this.state.headerStates[indexActiveHeader]){
+                return
+            } else{
+                let newHeaderStates = Array(this.state.headerStates.length).fill(false);
+                newHeaderStates[indexActiveHeader]=true
+                this.setState({headerStates: newHeaderStates})
+                }
+    }
+    render() {
 
-
-    render(){
-        const headerStates = [true, false, false, false, false, false]
-        return(
+        return (
             <>
 
-            <CustomHeader {...customcontractsheaderscards} color={"purple"} states={headerStates} />
+                <CustomHeader
+                    {...customcontractsheaderscards}
+                    color={"purple"}
+                    states={this.state.headerStates}
+                    clickable={true}
+                    onSelectCustomHeader={this.handleHeaderStates}
+                />
 
-                </>
+                <div className="flex justify-center mt-5">
+
+                    <div className="px-3 pb-5 mx-3 relative flex min-w-0 break-words bg-white rounded shadow-md ">
+                        <div className=" mt-2 fs-5 text-right text-blueGray-500 uppercase font-bold">
+                              {customcontractsinfo.info.texts.goals}
+                          </div>
+                    </div>
+
+                    <div className="px-3 mx-2 pb-5 mx-3 relative flex min-w-0 break-words bg-white rounded shadow-md ">
+                          <div className=" mt-2 fs-5 text-right text-blueGray-500 uppercase font-bold">
+                              {customcontractsinfo.info.texts.description}
+                          </div>
+                    </div>
+
+                </div>
+
+
+
+
+            </>
+
+
         )
     }
 }
