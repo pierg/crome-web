b144f50acd64cf6ba4d9d9b73564bf1bd1413a91
guillaume <gasser.guillaume@gmail.com>
2022-06-14 16:44:47 +0200

    Added very beautiful icons

--- frontend/src/_texts/custom/customcontractsheaderscards.js
+++ frontend/src/_texts/custom/customcontractsheaderscards.js
@@ -3,32 +3,33 @@ const textProps = {
   color: "lightBlue",
   cards: [{
       statTitle: "conjunction",
-      statIconName: "fas fa-circle-ampersand",
-    },
-    {
-      statTitle: "composition",
-      statIconName: "fas fa-bullseye",
+      statIconName: "fas fa-solid fa-plus",
       statIconColor: "bg-orange-500",
     },
     {
+      statTitle: "composition",
+      statIconName: "fas fa-solid fa-down-left-and-up-right-to-center",
+      statIconColor: "bg-pink-500",
+    },
+    {
       statTitle: "refinement",
-      statIconName: "fas fa-check-circle",
+      statIconName: "fas fa-solid fa-arrow-down-short-wide",
       statIconColor: "bg-lightBlue-500",
     },
           {
       statTitle: "quotient",
-      statIconName: "fas fa-check-circle",
-      statIconColor: "bg-lightBlue-500",
+      statIconName: "fas fa-solid fa-divide",
+      statIconColor: "bg-indigo-500",
     },
           {
       statTitle: "merging",
-      statIconName: "fas fa-check-circle",
-      statIconColor: "bg-lightBlue-500",
+      statIconName: "fas fa-solid fa-code-merge",
+      statIconColor: "bg-teal-500",
     },
       {
       statTitle: "separation",
-      statIconName: "fas fa-check-circle",
-      statIconColor: "bg-lightBlue-500",
+      statIconName: "fas fa-solid fa-arrows-left-right-to-line",
+      statIconColor: "bg-red-500",
     },
   ],
 };
