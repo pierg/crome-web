f6fb52d45dbe78acf7d17c208bff10ee295d5e48
David <plessis.david61@gmail.com>
2022-06-20 14:05:16 +0200

    working on contracts

--- frontend/src/components/Custom/ContractsChoiceActiveGoals.js
+++ frontend/src/components/Custom/ContractsChoiceActiveGoals.js
@@ -35,7 +35,8 @@ export default class ContractsChoiceActiveGoals extends React.Component {
             }
         }
         if (index !== -1) {
-            goalChoice = <>
+            goalChoice =
+                <div className="flex justify-evenly">
                 <UncontrolledDropdown>
                     <DropdownToggle
                         caret
@@ -69,32 +70,38 @@ export default class ContractsChoiceActiveGoals extends React.Component {
                         </DropdownItem>))}
                     </DropdownMenu>
                 </UncontrolledDropdown>
-            </>
+            </div>
         } else {
 
+            goalChoice =
+                <div className="grid grid-cols-2 gap-4">
+                    {this.props.goals.map((prop, key) => (
+                        <div key={key}
+                             className="flex items-center mb-4">
+                            <input
+                                onClick={() => this.selectGoals(prop)}
+                                id={key}
+                                type="checkbox"
+                                className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
+                            </input>
+                            <label htmlFor={key}
+                                   className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
+                                {prop.name}
+                            </label>
+                        </div>))
+                    }
+                </div>
         }
 
         return (
 
             <>
                 <div
-                    className="w-full mt-8 px-4 relative flex flex-col min-w-0 break-words bg-white rounded mb-6 opacity-1 transform duration-300 transition-all ease-in-out"> {this.props.goals.map((prop, key) => (
-                    <div key={key}
-                         className="flex items-center mb-4">
-                        <input
-                            onClick={() => this.selectGoals(prop)}
-                            id={key}
-                            type="checkbox"
-                            className="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                        </input>
-                        <label htmlFor={key}
-                               className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">
-                            {prop.name}
-                        </label>
-                    </div>))}
+                    className="w-full mt-8 px-4 relative flex flex-col min-w-0 break-words bg-white rounded mb-6 opacity-1 transform duration-300 transition-all ease-in-out">
 
 
                     {goalChoice}
+
                     <div className="flex content-center justify-center">
                         <ElementsButton
                             size="sm" color="facebook" outline={false}
