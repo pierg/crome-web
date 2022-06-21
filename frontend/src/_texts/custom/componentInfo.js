f4a4e1d49d19b6fbc00c8cfebf4bdbb4c8bd9149
Linot <thealinot@gmail.com>
2022-06-21 12:25:51 +0200

    info

--- frontend/src/_texts/custom/componentInfo.js
+++ frontend/src/_texts/custom/componentInfo.js
@@ -3,7 +3,11 @@ const textProps = {
         texts: {
             title: "New page",
             diagram: "Diagram"
+        },
+        icon: {
+            build: "fas fa-2xl fa-arrow-right"
         }
+
     }
 }
 
