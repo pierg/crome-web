356022d7d161d0316df814a48dc8e8e1c613d5ca
guillaume <gasser.guillaume@gmail.com>
2022-06-21 15:37:54 +0200

    fix issues

--- frontend/src/components/Custom/ContractsChoiceActiveGoals.js
+++ frontend/src/components/Custom/ContractsChoiceActiveGoals.js
@@ -73,7 +73,7 @@ export default class ContractsChoiceActiveGoals extends React.Component {
 
                         <label htmlFor="firstDropDownMenu"
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
-                            choose your {contractschoiceactivegoals.operation[index].firstDropDownMenuChoice}
+                            Choose your {contractschoiceactivegoals.operation[index].firstDropDownMenuChoice}
                         </label>
                         <select id="firstDropDownMenu"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
@@ -102,7 +102,7 @@ export default class ContractsChoiceActiveGoals extends React.Component {
 
                         <label htmlFor="firstDropDownMenu"
                                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">
-                            choose your {contractschoiceactivegoals.operation[index].secondDropDownMenuChoice}
+                            Choose your {contractschoiceactivegoals.operation[index].secondDropDownMenuChoice}
                         </label>
                         <select id="firstDropDownMenu"
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
