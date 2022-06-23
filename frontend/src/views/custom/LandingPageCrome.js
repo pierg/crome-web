6ce292d9b69608aec73452de40571193d92a196d
guillaume <gasser.guillaume@gmail.com>
2022-06-23 16:20:26 +0200

    fix warnig

--- frontend/src/views/custom/LandingPageCrome.js
+++ frontend/src/views/custom/LandingPageCrome.js
@@ -16,7 +16,7 @@ export default class LandingPageCrome extends React.Component {
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </div>
                         <div className="flex justify-center">
-                            <Link to="/crome" class="nav-link">
+                            <Link to="/crome" className="nav-link">
                                 <Button color="white" outline={true} size="customLg">
                                 <span className="text-lg">
                                     Start Crome
