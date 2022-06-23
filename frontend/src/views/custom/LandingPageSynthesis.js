79a15763b7dcff92e49eab22ef3f292f1ae15f7f
Théa <thealinot@gmail.com>
2022-06-23 12:08:53 +0200

    landing page for synthesis finish

--- frontend/src/views/custom/LandingPageSynthesis.js
+++ frontend/src/views/custom/LandingPageSynthesis.js
@@ -10,7 +10,7 @@ export default class LandingPageSynthesis extends React.Component {
                 <div className="flex justify-center min-h-screen">
                     <div className="flex flex-col justify-center w-50">
                         <div className="display-1 title-up text-white text-shadow-md mb-8 text-center font-bold">
-                            Crome
+                            Synthesis
                         </div>
                         <div className="text-base text-white mb-12 text-shadow-md text-justify italic">
                             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
@@ -18,9 +18,10 @@ export default class LandingPageSynthesis extends React.Component {
                         <div className="flex justify-center">
                             <Link to="/synthesis" class="nav-link">
                                 <Button color="white" outline={true} size="lg">
-                                <span className="text-lg">
-                                    Start Crome
-                                </span>
+                                    <span className="text-lg">
+                                        Start Synthesis
+                                    </span>
+                                    <i className="ml-2 text-lg fa-solid fa-arrow-right"></i>
                                 </Button>
                             </Link>
                         </div>
